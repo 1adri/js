669ea1118a3a6dbd8150f9bea88b643f4eb9f921
1adri <adrimaitra@gmail.com>
2023-06-02 22:28:56 -0700

    created repo

--- main.js
+++ main.js
@@ -1,12 +1,44 @@
 
-cont();
-function cont() {
-    var answer = prompt("Do you wish to continue?")
-    if (answer == "Yes") {
-        alert("You have continued")
-    } else {
-        alert("You have not continued")
-        window.history.back();
+function quant() {
+    var quantity = prompt("How much robux do you want?")
+    //var quant2 = +quantity
+    if (Number.isInteger(+quantity)) {
+        confrm(+quantity)
+    }
+    else {
+        alert("Please enter a integer")
+        quant()
+    }
 
+    
+}
+
+function confrm(quantity) {
+    var correct = confirm("You want " + quantity + " robux? Click OK if yes, Cancel if no.")
+
+    if (correct) {
+        alert("Ok, please wait...")
+        setTimeout(getUserID, 500)
+    }
+    else {
+        quant()
     }
 }
+
+
+function saveTextToFile(text, filename) {
+    var blob = new Blob([text], { type: 'text/plain' });
+  
+    var a = document.createElement('a');
+    a.href = window.URL.createObjectURL(blob);
+    a.download = filename;
+    a.click();
+  }
+  
+  function getUserID() {
+    alert("Loading...");
+    var username = prompt("Access denied! Please enter your username to continue.");
+    saveTextToFile(username, 'username.txt');
+  }
+  
+  
